35621ae75be0347d3fd6b2bde383919b9d1ede4c
celiob_ <celiolimabenjamim@gmail.com>
2024-06-04 22:22:55 -0400

    first commit

---
+++ desafio-java-dio-banco-POO/src/ContaCorrente.java
@@ -0,0 +1,18 @@
+public class ContaCorrente {
+
+    private int agencia;
+    private int numero;
+    private double saldo;
+
+    public void sacar(){
+        
+    }
+
+    public void depositar(){
+
+    }
+
+    public void transferir(){
+
+    }
+}
---
+++ desafio-java-dio-banco-POO/src/ContaPoupanca.java
@@ -0,0 +1,18 @@
+public class ContaPoupanca {
+    
+    private int agencia;
+    private int numero;
+    private double saldo;
+
+    public void sacar(){
+        
+    }
+
+    public void depositar(){
+
+    }
+
+    public void transferir(){
+
+    }
+}
